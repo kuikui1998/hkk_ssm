0b8fa6996544f14967328c02e4817d1b1b845aa4
kuikui1998 <shakuil@qq.com>
2020-07-14 15:38:42 +0800

    整合domain模块

---
+++ hkk_ssm_domain/src/main/java/com/hkk/ssm/domain/Member.java
@@ -0,0 +1,56 @@
+package com.hkk.ssm.domain;
+
+/**
+ * @auther kuikui
+ * @create 2020-07-13 17:23
+ *
+ * 会员
+ */
+public class Member {
+
+    private Integer id;
+    private String name;
+    private String nickname;
+    private String phoneNum;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
