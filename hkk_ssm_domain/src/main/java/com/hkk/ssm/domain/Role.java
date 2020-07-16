736e5459d894e83a7c32431ab8592ebb494807c4
kuikui1998 <shakuil@qq.com>
2020-07-16 23:13:52 +0800

    权限管理domain

---
+++ hkk_ssm_domain/src/main/java/com/hkk/ssm/domain/Role.java
@@ -0,0 +1,57 @@
+package com.hkk.ssm.domain;
+
+
+import java.util.List;
+
+/**
+ * @auther kuikui
+ * @create 2020-07-14 16:46
+ */
+public class Role {
+
+    private Integer id;
+    private String roleName;
+    private String roleDesc;
+    private List<Permission> permissions;
+    private List<UserInfo> users;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDesc() {
+        return roleDesc;
+    }
+
+    public void setRoleDesc(String roleDesc) {
+        this.roleDesc = roleDesc;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public List<UserInfo> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserInfo> users) {
+        this.users = users;
+    }
+}
